0d3239f86ac6d221c43304f150adde7ba8f38b65
Kent Yucra <mckkentmwk@gmail.com>
2021-05-26 23:11:25 -0500

    migrations and model

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+
+class TestPost(TestCase):
+
+    def setUp(self) -> None:
+        self.post = Post.objects.create(title='django', author='django', slug='django')
+
+    def test_post_model(self):
+        self.assertEqual(str(self.post), 'django')
